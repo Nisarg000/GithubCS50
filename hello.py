7ceee26fc01d803e9a4508d4fb9c80d1c89e6765
Nisarg000 <@nisargjhatakia94@gmail.com>
2021-11-23 16:32:41 +0530

    I create a new branch using  git checkout -b add-comments and added only a single comment and removed the rest of the comments so lets see if that overwrites my previous file

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
 print("Hello World")
-# https://stackoverflow.com/questions/51333804/rsa-public-key-no-such-file-or-directory
-# that above link saved me from the fucking headache of using ssh keys
-#then I added my ssh key to github and it worked!
+# The above line shall print hello
